990cd8d9787c52d24ae579a0f1d90e1f2e7cec8e
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-25 14:27:15 +0530

    concatenate commit

---
+++ python_operators/Assignment_Operators.py
@@ -0,0 +1,36 @@
+x = 5	
+x += 3
+print(x)
+x = 5
+x -= 3	
+print(x)
+x = 5	
+x *= 3	
+print(x)
+x = 5
+x /= 3	
+print(x)
+x = 5 	
+x %= 3
+print(x)
+x = 5	
+x //= 3	
+print(x)
+x = 5	
+x **= 3	
+print(x)
+x = 5
+x &= 3	
+print(x)
+x = 5
+x |= 3	
+print(x)
+x = 5	
+x ^= 3	
+print(x)
+x = 5 
+x >>= 3	
+print(x)
+x = 5 
+x <<= 3	
+print(x)
